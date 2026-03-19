8200d5d7dec00bb90bddf44c44da7703237dde99
Sourav Debnath <sdsouravdebnath26@gmail.com>
2026-03-19 15:51:04 +0600

    removed all

--- src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { JwtUser } from '../types/auth.types';
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext): JwtUser => {
-    const request = ctx.switchToHttp().getRequest<{ user: JwtUser }>();
-    return request.user;
-  },
-);
